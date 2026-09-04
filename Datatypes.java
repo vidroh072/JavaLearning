a15a1c8bf630faf5726fa3633c6737978edb509c
vidroh072 <aashishpatidar074@gmail.com>
2026-09-04 12:08:57 +0530

    1.2

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+class Datatypes {
+    public static void main(Strings a[]){
+        // Primitive
+        // Integer, float, character, boolean
+        
+        int num1 = 50;   // 4 bytes (-2^31)-(2^31-1)
+        long num2 = 450L; // 8 bytes (-2^63)-(2^63-1)
+        short num3 = 50; // 2 bytes (-2^15)-(2^15-1)
+        byte num4 = 127; // 1 bytes (-2^7)-(2^7-1)
+        
+        float num5 = 125.54f; // 4 bytes
+        double num6 = 125.54; // 8 bytes *default
+        
+        char ch = 'a'; // 2 bytes
+
+        boolean isTrue = true;
+
+        // literals
+
+        int number = 50_00_000;
+
+    }    
+}
