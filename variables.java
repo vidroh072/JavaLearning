bd2e91f463f114cf65320011c5eb643c572f8e7e
vidroh072 <aashishpatidar074@gmail.com>
2026-09-04 12:08:21 +0530

    1.1

---
+++ variables.java
@@ -0,0 +1,9 @@
+class variables{
+    public static void main(Strings a[]){
+        int marks = 50; //--> variable
+        //  int -> datatype marks -> variable name
+        System.out.print(marks);
+
+        // variables are container -> temporary storage
+    }
+}
